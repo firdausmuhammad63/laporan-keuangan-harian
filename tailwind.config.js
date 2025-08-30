864038c27c1a3067ba7839b02e12a74a300261bd
firdausmuhammad63 <daoes700@gmail.com>
2025-08-30 07:15:23 +0700

    laporan-keuangan-harian

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./*.html",
+    "./src/**/*.html",
+    "./src/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4f46e5",
+        secondary: "#10b981"
+      }
+    },
+  },
+  plugins: [],
+}
